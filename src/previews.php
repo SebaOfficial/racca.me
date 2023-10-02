33f51c78073ec31356e6a5cb830b41520fe6ddc7
Sebastiano <sebastiano@racca.me>
2023-10-02 19:31:29 +0200

    Modified previews' concept for indexes

--- src/previews.php
+++ src/previews.php
@@ -5,7 +5,7 @@ require_once __DIR__ . "/envirorment.php";
 $pages = PHPStatic\Sys::getDir(ROOT_DIR . "/dist/", ["html"]);
 
 foreach ($pages as $page) {
-    list($lang, $name) = explode('/', PHPStatic\Builder::removeExtension($page));
+    list($lang, $name) = explode('/', str_replace("index", "", PHPStatic\Builder::removeExtension($page)));
 
     exec("wkhtmltoimage --width 1024 --height 768 --javascript-delay 2000 'https://racca.me/$page' 'dist/assets/previews/$lang-$name.png'");
 }
