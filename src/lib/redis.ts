0c771264dbf0e7b43150545af01221be87f0b12a
Sebastiano <sebastiano@racca.me>
2024-09-25 13:14:46 +0200

    Fix redis connection

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,19 +1,13 @@
 import { createClient } from 'redis';
 import { REDIS_HOST, REDIS_PASSWORD } from '$env/static/private';
 
-let redis;
+const redis = createClient({
+	url: REDIS_HOST,
+	password: REDIS_PASSWORD
+});
 
-if (process.env.BUILD_ENV === 'build') {
-	console.log('Skipping Redis connection during the build process');
-} else {
-	redis = createClient({
-		url: REDIS_HOST,
-		password: REDIS_PASSWORD
-	});
-
-	await redis.connect().catch((err) => {
-		console.error('Failed to connect to Redis', err);
-	});
-}
+await redis.connect().catch((err) => {
+	console.error('Failed to connect to Redis', err);
+});
 
 export default redis;
